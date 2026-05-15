b5902b8992bf43bb3f3861f963cdc0942ee7c956
jhotlann <joelhotlan@gmail.com>
2026-05-15 20:49:23 +0700

    feat: pool windows

--- src/cnn/layers/pooling2d.py
+++ src/cnn/layers/pooling2d.py
@@ -1,56 +1,50 @@
 import numpy as np
 
 
+def _pool_windows(x, pool_size, stride):
+    """
+    output shape: [B, H_out, W_out, pH, pW, C]
+    """
+    B, H, W, C = x.shape
+    pH, pW = pool_size
+    sH, sW = stride
+
+    H_out = (H - pH) // sH + 1
+    W_out = (W - pW) // sW + 1
+
+    windows = np.lib.stride_tricks.as_strided(
+        x,
+        shape=(B, H_out, W_out, pH, pW, C),
+        strides=(
+            x.strides[0],
+            x.strides[1] * sH,
+            x.strides[2] * sW,
+            x.strides[1],
+            x.strides[2],
+            x.strides[3],
+        )
+    )
+    return windows
+
+
 class MaxPooling2D:
     def __init__(self, keras_layer):
         self.pool_size = keras_layer.pool_size
-        self.stride = keras_layer.strides
+        self.stride    = keras_layer.strides
 
     def forward(self, x):
-        '''
-        x shape: [B, H, W, C]
-        y shape: [B, H_out, W_out, C]
-        '''
-        B, H, W, C = x.shape
-        pH, pW = self.pool_size
-        sH, sW = self.stride
-
-        H_out = (H - pH) // sH + 1
-        W_out = (W - pW) // sW + 1
-
-        y = np.zeros((B, H_out, W_out, C), dtype=np.float32)
-        for i in range(H_out):
-            for j in range(W_out):
-                patch = x[:, i*sH:i*sH+pH, j*sW:j*sW+pW, :]  # [B, pH, pW, C]
-                y[:, i, j, :] = patch.max(axis=(1, 2))
-
-        return y
+        windows = _pool_windows(x, self.pool_size, self.stride)
+        return windows.max(axis=(3, 4))
 
 
 class AveragePooling2D:
     def __init__(self, keras_layer):
         self.pool_size = keras_layer.pool_size
-        self.stride = keras_layer.strides
+        self.stride    = keras_layer.strides
 
     def forward(self, x):
-        '''
-        x shape: [B, H, W, C]
-        y shape: [B, H_out, W_out, C]
-        '''
-        B, H, W, C = x.shape
-        pH, pW = self.pool_size
-        sH, sW = self.stride
-
-        H_out = (H - pH) // sH + 1
-        W_out = (W - pW) // sW + 1
-
-        y = np.zeros((B, H_out, W_out, C), dtype=np.float32)
-        for i in range(H_out):
-            for j in range(W_out):
-                patch = x[:, i*sH:i*sH+pH, j*sW:j*sW+pW, :]
-                y[:, i, j, :] = patch.mean(axis=(1, 2))
-
-        return y
+        windows = _pool_windows(x, self.pool_size, self.stride)
+        return windows.mean(axis=(3, 4))
 
 class GlobalAveragePooling2D:
     def __init__(self, keras_layer=None):
