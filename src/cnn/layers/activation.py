12555a51f516e5252253ecf97f7f2b9e2bae5483
jhotlann <joelhotlan@gmail.com>
2026-05-15 16:17:30 +0700

    chores: delete unused activation

--- src/cnn/layers/activation.py
+++ src/cnn/layers/activation.py
@@ -25,21 +25,12 @@ class Linear:
     def __call__(self, x):
         return x
 
-class LeakyReLU:
-    def __init__(self, alpha=0.3):
-        """alpha: negative slope, matches Keras default (0.3)."""
-        self.alpha = alpha
-
-    def __call__(self, x):
-        return np.where(x >= 0, x, self.alpha * x)
-
 ACTIVATION_MAP = {
     "relu":         ReLU,
     "sigmoid":      Sigmoid,
     "tanh":         Tanh,
     "softmax":      Softmax,
     "linear":       Linear,
-    "leaky_relu":   LeakyReLU,
 }
 
 
