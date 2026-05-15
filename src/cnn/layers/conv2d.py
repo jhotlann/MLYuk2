f2e7454c591c66a4708f322591e6fdc03fee3806
jhotlann <joelhotlan@gmail.com>
2026-05-15 20:21:45 +0700

    refactor: optimize forwrard

--- src/cnn/layers/conv2d.py
+++ src/cnn/layers/conv2d.py
@@ -19,47 +19,42 @@ class Conv2D:
         self.kH, self.kW, self.C_in, self.C_out = self.kernel.shape
 
     def forward(self, x):
-        '''
-        x shape: [B, H, W, C_in]
-        y shape: [B, H_out, W_out, C_out]
-        '''
         kH, kW, C_in, C_out = self.kernel.shape
         B, H, W, C_x = x.shape
         sH, sW = self.stride
 
         if C_in != C_x:
-            raise ValueError(
-                f"Input channels ({C_x}) "
-                f"!= kernel channels ({C_in})"
-            )
+            raise ValueError(f"Input channels ({C_x}) != kernel channels ({C_in})")
 
         if self.padding == "same":
             pad_h = max(kH - 1, 0)
             pad_w = max(kW - 1, 0)
-            pad_top, pad_bottom = pad_h // 2, pad_h - pad_h // 2
-            pad_left, pad_right = pad_w // 2, pad_w - pad_w // 2
-
-            # NumPy padding
-            x = np.pad(x, ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right), (0, 0)), mode='constant')
+            pad_top,    pad_bottom = pad_h // 2, pad_h - pad_h // 2
+            pad_left,   pad_right  = pad_w // 2, pad_w - pad_w // 2
+            x = np.pad(x, ((0,0),(pad_top,pad_bottom),(pad_left,pad_right),(0,0)), mode='constant')
             H, W = x.shape[1], x.shape[2]
 
         h_out = (H - kH) // sH + 1
         w_out = (W - kW) // sW + 1
 
-        y = np.zeros((B, h_out, w_out, C_out), dtype=np.float32)
-        for b in range(B):
-            for out_c in range(C_out):
-                y_in = np.zeros((h_out, w_out), dtype=np.float32)
-                for in_c in range(C_in):
-                    x_in = x[b, :, :, in_c]
-                    k_in = self.kernel[:, :, in_c, out_c]
+        col = np.lib.stride_tricks.as_strided(
+            x,
+            shape=(B, h_out, w_out, kH, kW, C_in),
+            strides=(
+                x.strides[0],
+                x.strides[1] * sH,
+                x.strides[2] * sW,
+                x.strides[1],
+                x.strides[2],
+                x.strides[3],
+            )
+        ).reshape(B, h_out * w_out, kH * kW * C_in)
 
-                    for i in range(h_out):
-                        for j in range(w_out):
-                            y_in[i, j] += (
-                                x_in[i*sH:i*sH+kH, j*sW:j*sW+kW] * k_in
-                            ).sum()
-                y_in += self.bias[out_c]
-                y[b, :, :, out_c] = self.activation(y_in)
+        # kernel shape: [kH*kW*C_in, C_out]
+        k = self.kernel.reshape(-1, C_out)
 
-        return y
+        # matrix multiply: [B, h_out*w_out, C_out]
+        y = (col @ k) + self.bias
+        y = y.reshape(B, h_out, w_out, C_out)
+
+        return self.activation(y)
