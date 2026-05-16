69ce9cc67a40a18dde0b92a28f2be0d5905dfb2d
jhotlann <joelhotlan@gmail.com>
2026-05-16 14:38:06 +0700

    fix: kernel weight

--- src/cnn/layers/locallyconnected2d.py
+++ src/cnn/layers/locallyconnected2d.py
@@ -5,7 +5,6 @@ class LocallyConnected2D:
     def __init__(self, keras_layer):
         self.kernel      = keras_layer._lc_kernel
         self.bias        = keras_layer._lc_bias
-        self.is_shared   = keras_layer._is_shared_weight
         self.kH, self.kW = keras_layer.kernel_size
         self.C_out       = self.kernel.shape[-1]
         self.activation  = get_activation(keras_layer)
@@ -30,17 +29,19 @@ class LocallyConnected2D:
 
         H_out = (H - kH) // sH + 1
         W_out = (W - kW) // sW + 1
+        n_pos = H_out * W_out
+
+        if self.kernel.ndim == 2:
+            self.kernel = np.tile(self.kernel[np.newaxis], (n_pos, 1, 1))  # (n_pos, kH*kW*C_in, C_out)
+            self.bias   = np.tile(self.bias[np.newaxis],   (n_pos, 1))     # (n_pos, C_out)
+
         y = np.zeros((B, H_out, W_out, self.C_out), dtype=np.float32)
 
         pos = 0
         for i in range(H_out):
             for j in range(W_out):
                 patch = x[:, i*sH:i*sH+kH, j*sW:j*sW+kW, :].reshape(B, -1)
-
-                if self.is_shared:
-                    y[:, i, j, :] = patch @ self.kernel + self.bias
-                else:
-                    y[:, i, j, :] = patch @ self.kernel[pos] + self.bias[pos]
-                    pos += 1
+                y[:, i, j, :] = patch @ self.kernel[pos] + self.bias[pos]
+                pos += 1
 
         return self.activation(y)
